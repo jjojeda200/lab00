d173589654957f365b0d3ed33dbe141d7115da97
José Juan Ojeda Granados <jotafirebase@gmail.com>
2024-06-14 20:23:17 +0100

    ok

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 function ref() {
   root.render(<App />);
+  //ReactDOM.render(<App />, document.getElementById('root'));
 }
 
 setInterval(ref, 1000);
