7bc12ae8612f71298d61e42ca7752a947f3ca3fa
José Juan Ojeda Granados <jotafirebase@gmail.com>
2024-06-20 23:40:27 +0100

    ok

--- src/index00.js
+++ src/index00.js
@@ -1,18 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Lab00 from "./componentes/Lab00";
-// import {Lab01, Lab02 } from "./componentes/Lab01";
-import GaleriaDeImagenes from "./Pildoras/pi05"
+import {Lab01, Lab02 } from "./componentes/Lab01";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// const lab01 = ReactDOM.createRoot(document.getElementById("Lab01"));
-// const lab02 = ReactDOM.createRoot(document.getElementById("Lab02"));
-const galeria = ReactDOM.createRoot(document.getElementById("galeria"));
+const lab01 = ReactDOM.createRoot(document.getElementById("Lab01"));
+const lab02 = ReactDOM.createRoot(document.getElementById("Lab02"));
 function ref() {
   root.render(<Lab00 />);
-  galeria.render(<GaleriaDeImagenes />);
-  // lab01.render(<Lab01 />);
-  // lab02.render(<Lab02 />);
+  lab01.render(<Lab01 />);
+  lab02.render(<Lab02 />);
   
   //ReactDOM.render(<App />, document.getElementById('root'));
 }
