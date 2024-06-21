1173aa2f750a952636981cd316a219f0cee5d6e0
José Juan Ojeda Granados <jotafirebase@gmail.com>
2024-06-21 20:48:29 +0100

    ok

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,7 @@ module.exports = function override(config, env) {
   //config.entry = path.resolve(__dirname, './src/Pildoras/App.js');
   //return config;
 
-  const entryPath = process.env.REACT_APP_ENTRY || 'src/index00.js';
+  const entryPath = process.env.REACT_APP_ENTRY || 'src/index.js';
   config.entry = path.resolve(__dirname, entryPath);
   return config;
 };
---
+++ src/FernandoH/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Lab00 from "./componentes/Lab00";
+// import {Lab01, Lab02 } from "./componentes/Lab01";
+import GaleriaDeImagenes from "./Pildoras/pi05"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// const lab01 = ReactDOM.createRoot(document.getElementById("Lab01"));
+// const lab02 = ReactDOM.createRoot(document.getElementById("Lab02"));
+const galeria = ReactDOM.createRoot(document.getElementById("galeria"));
+function ref() {
+  root.render(<Lab00 />);
+  galeria.render(<GaleriaDeImagenes />);
+  // lab01.render(<Lab01 />);
+  // lab02.render(<Lab02 />);
+  
+  //ReactDOM.render(<App />, document.getElementById('root'));
+}
+
+setInterval(ref, 1000);
